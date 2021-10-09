864ee07787052674c6164ba169e0cfc3a1d002c8
albunus <albunus.nyalita@student.moringaschool.com>
2021-10-09 22:26:49 +0300

    it will updatethe quote class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,15 @@
 export class Quote {
+  id: number;
+  name: string;
+  quoteName : string;
+  last : string;
+  showAuthorName: boolean;
+
+  constructor (id: number, name: string, quoteName: string, last: string, public submitDate: Date) {
+      this.id = id,
+      this.name =name,
+      this.quoteName = quoteName,
+      this.last = last,
+      this.showAuthorName = false
+  }
 }
